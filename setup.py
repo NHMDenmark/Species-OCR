036905c3b68ca8e7438e71ff413ef06c9c21eb8d
Aksel Birkø <akselb@live.dk>
2023-01-22 15:54:31 +0100

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
-from setuptools import setup
-setup(name='nhma_species_ocr', version="1.0.0")
+from setuptools import setup, find_packages
+setup(
+    name='nhma_species_ocr', 
+    version="1.0.0",
+    packages=find_packages(),
+    package_dir={'': '.'}
+)
