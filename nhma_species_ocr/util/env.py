f13943e92ac1093ac04dcd4a656db3451364e475
Aksel Birkø <akselb@live.dk>
2023-08-25 00:28:34 +0200

    Fix OS dependent paths for .env file

--- nhma_species_ocr/util/env.py
+++ nhma_species_ocr/util/env.py
@@ -1,5 +1,5 @@
-import os
 from decouple import Config, RepositoryEnv
+from pathlib import Path
 
 
-env_config = Config(RepositoryEnv(os.path.join(os.path.dirname(__file__), '../../.env')))
+env_config = Config(RepositoryEnv(Path(__file__).parent.parent.parent.joinpath('.env')))
