c73edf1b7062723cbe73b41257f1d549bee7fb1c
Aksel Birkø <akselb@live.dk>
2023-10-16 22:01:09 +0200

    update is_cover to search in top of specimen

--- nhma_species_ocr/is_cover/is_cover.py
+++ nhma_species_ocr/is_cover/is_cover.py
@@ -6,14 +6,14 @@ from nhma_species_ocr.util.variables import cover_detection_scale
 def is_cover(img) -> bool:
     """
     Method that takes an image, and determines whether it is a cover of a species collection.
-    Works by trying to detect a data matrix that species are marked with.
+    Works by trying to detect a data matrix that specimen are marked with.
 
     Args:
         img: image to process
     """
     scale = cover_detection_scale / 100
     img = cv2.resize(img, (0, 0), fx=scale, fy=scale)
-    img = img[(img.shape[0]-img.shape[0]/4).__round__():, :(img.shape[1]/1).__round__()]
+    img = img[:(img.shape[0]/5).__round__(), :(img.shape[1]/1).__round__()]
 
     decoding = decode(img, max_count=1, timeout=2000)
     return not decoding
--- nhma_species_ocr/util/variables.py
+++ nhma_species_ocr/util/variables.py
@@ -2,7 +2,7 @@ from decouple import config, UndefinedValueError
 
 
 # Required
-try :
+try:
     image_folder = config('IMAGE_FOLDER')
     label_folder = config('LABEL_FOLDER')
     label_threshold_folder = config('LABEL_THRESHOLD_FOLDER')
@@ -11,13 +11,14 @@ try :
 
     web_host = config('WEB_HOST')
 except UndefinedValueError as e:
-    raise Exception(f"Required environment variable '{e.__str__().split(' ')[0]}' not found. Reference the .env.example file of this project for required variables")
+    raise Exception(
+        f"Required environment variable '{e.__str__().split(' ')[0]}' not found. Reference the .env.example file of this project for required variables")
 
 # Optional
 dilation_rect_size = config('FIND_COVER_LABEL_DILATION_RECT_SIZE', default=8, cast=int)
 label_scale = config('LABEL_SCALE_PERCENT', default=40, cast=int)
 label_extra_border = config('LABEL_EXTRA_BORDER_PIXELS', default=100, cast=int)
-cover_detection_scale = config('COVER_DETECTION_SCALE_PERCENT', default=50, cast=int)
+cover_detection_scale = config('COVER_DETECTION_SCALE_PERCENT', default=20, cast=int)
 threshold_block_size = config('LABEL_THRESHOLD_BLOCK_SIZE', default=91, cast=int)
 threshold_subtract_constant = config('LABEL_THRESHOLD_SUBTRACT_CONSTANT', default=18, cast=int)
 dev_only_covers = config('DEV_ONLY_COVERS', default=False, cast=bool)
